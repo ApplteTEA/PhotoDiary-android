b3f4fff79cd4424d31a5b03f15b80e2b85ac4abb
jinwon.seo <jinwon.seo@homeplus.co.kr>
2026-03-23 09:21:45 +0900

    test

--- app/src/main/java/com/example/photodiary/MonthlyReflectionScreen.kt
+++ app/src/main/java/com/example/photodiary/MonthlyReflectionScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Alignment
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -42,6 +41,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
